d6fd9391daec45ca4a55d80ea07d595ae7653735
Arekh Shrestha <sthaarekh@Arekhs-MacBook-Air.local>
2026-06-23 23:20:11 +0545

    Updated status

--- src/data/stats.tsx
+++ src/data/stats.tsx
@@ -8,4 +8,5 @@ export const statsData: StatItem[] = [
   { id: 1, value: "95", label: "Student Partners Enrolled" },
   { id: 2, value: "80+", label: "Knowledge Sharing Sessions Delivered" },
   { id: 3, value: "5", label: "Student Parteners Hired" }
+  { id: 4, value: "2100+", label: "Students Impacted by LSPs" }
 ];
